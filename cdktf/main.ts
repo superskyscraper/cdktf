715e80a9f0c7291735bcec72cdb70693f642fb85
superskyscraper <87255059+superskyscraper@users.noreply.github.com>
2022-11-23 06:48:40 +0000

    prettier追加

--- cdktf/main.ts
+++ cdktf/main.ts
@@ -1,30 +1,30 @@
 // Copyright (c) HashiCorp, Inc
 // SPDX-License-Identifier: MPL-2.0
-import { Construct } from "constructs";
-import { App, TerraformStack, TerraformOutput } from "cdktf";
-import { AwsProvider } from "@cdktf/provider-aws/lib/provider";
-import { Instance } from "@cdktf/provider-aws/lib/instance";
+import { Construct } from 'constructs';
+import { App, TerraformStack, TerraformOutput } from 'cdktf';
+import { AwsProvider } from '@cdktf/provider-aws/lib/provider';
+import { Instance } from '@cdktf/provider-aws/lib/instance';
 
 class MyStack extends TerraformStack {
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
     // define resources here
-    new AwsProvider(this, "AWS", {
-      region: "ap-northeast-1",
+    new AwsProvider(this, 'AWS', {
+      region: 'ap-northeast-1'
     });
 
-    const ec2Instance = new Instance(this, "ec2", {
-      ami: "ami-072bfb8ae2c884cc4",
-      instanceType: "t2.micro",
+    const ec2Instance = new Instance(this, 'ec2', {
+      ami: 'ami-072bfb8ae2c884cc4',
+      instanceType: 't2.micro'
     });
 
-    new TerraformOutput(this, "public_ip", {
-      value: ec2Instance.publicIp,
+    new TerraformOutput(this, 'public_ip', {
+      value: ec2Instance.publicIp
     });
   }
 }
 
 const app = new App();
-new MyStack(app, "cdktf");
+new MyStack(app, 'cdktf');
 app.synth();
